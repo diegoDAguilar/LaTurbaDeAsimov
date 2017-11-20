a61805b04c0dd863dc3bd18e702525235c69a3df
diegoDAguilar <diego.deaguilarcanellas@telefonica.com>
2017-11-20 17:18:46 +0100

    supermatrix added

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import sys
 import os
 
 
-
+des_list=[]
+des_array=[]
 def extractor(directory, nodo, sizeX = 800, sizeY = 400):
-    
+    global des_list
+    global des_array
     orb = cv2.ORB_create()
     image_list = os.listdir(directory)
-    
+    des_array=np.zeros((1,32))
+    #supermatriz 
     for i in range(len(image_list)) :
         #Reads image and sets its size to preset
         file_name = directory + image_list[i]
@@ -21,19 +24,13 @@ def extractor(directory, nodo, sizeX = 800, sizeY = 400):
         #Generates a gray image 
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         gray = np.float32(gray)
-        kp = orb.detect(img,None)
-        kp, des = orb.compute(img, kp)
-    
-        img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
-    
         
-        #stores image with the orb markers in a new file
-        histograms_directory = os.getcwd() + '\Histogram'
-        if not os.path.exists(histograms_directory):
-            os.makedirs(histograms_directory)
-        save_directory = os.getcwd() + '\Histogram\\' + nodo + str(i) + '.png'
-        #print(save_directory)
-        cv2.imwrite(save_directory, img2)
+        # find the keypoints with ORB
+        kp = orb.detect(img,None)
+        # compute the descriptors with ORB
+        kp, des = orb.compute(img, kp)
+        des_list.append(des)
+        des_array=np.vstack((des_array,des))
 
 def codec(clusters,des_matrix,des_list,n_images_per_node):
     #clusters: Number of clusters
@@ -75,8 +72,11 @@ def main(argv):
     print(working_dir)
     for i in range(1,int(sys.argv[1]) + 1):
         extractor(directory = working_dir + '\Dataset\Nodo' + str(i) + '\\', nodo='Nodo' + str(i) + '_')
-        
+   
+    #tratamiento de la supermatriz
+    #
 
 if __name__ == "__main__":
-    main(sys.argv)
-    
+    main(sys.argv) 
+
+
