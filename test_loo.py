8989d9fa6d512df7cbabb414748166dffdc5937f
aprator <32907030+aprator@users.noreply.github.com>
2017-11-21 18:04:54 +0100

    Final

--- test_loo.py
+++ test_loo.py
@@ -7,35 +7,48 @@ Created on Sun Nov 19 20:12:06 2017
 
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import LeaveOneOut
 
 
 # Read training set
-#testfile = np.load(outfile)
-histogram = np.array([[1, 2], [3, 4], [5,6], [6,7]])
-labels = np.array([1, 2, 2, 1])
+training_set = np.load('training_set.npz')
+clusters = training_set['cluster']
+training_set = training_set['hist_matrix']
+
+#Parameters
+histograms = training_set[:,0:clusters]
+labels = training_set[:,clusters]
+test_size=len(histograms) 
+tp=0 # True-positives
+
 
 #Initialize leave-one-out
 loo = LeaveOneOut()
-test_size=len(histogram) #Innecesario pero lo dejo por ahora
 
-
-for train_index, test_index in loo.split(histogram):
+for train_index, test_index in loo.split(histograms):
     #Initialize classifier
-    clf=SVC()
+    clf=LinearSVC()
     
     #Train classifier
-    histogram_train = histogram[train_index] 
+    histogram_train = histograms[train_index] 
     labels_train = labels[train_index]
     clf.fit(histogram_train, labels_train) 
-    
+
     #Test classifier    
-    histogram_test = histogram[test_index]
+    histogram_test = histograms[test_index]
     labels_test = labels[test_index]
     result = clf.predict(histogram_test)
-    if (result == labels_test):
-        test_size-=1
     
+    if (result == labels_test):
+        tp+=1
+    else:
+        print test_index
+        print 'FALLO -> resultado:', result, 'label:', labels_test
+
+# Calculate accuracy       
+accuracy = tp*100/test_size
+print 'Precisión:',accuracy,'%'
 
 
 
