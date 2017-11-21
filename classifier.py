28dd36b99b66284447acaec7322f94f680126dfd
Serge Chávez <30761241+Serge3006@users.noreply.github.com>
2017-11-21 19:11:51 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -10,10 +10,15 @@ from sklearn.cluster import KMeans
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
 
+des_list = []  
+des_array = np.zeros((1,32))
+
 '''Main'''
-# Read training set
-des_list, des_array = extractor(node_directory, des_list, des_array, cod_op = 1)
+# Reading training set
+des_list, des_array = extractor(node_directory, des_list, des_array, cod_op)
+des_array = np.delete(des_array,0,0)
+# Loading the Bag of Words
+BoW = joblib.load('BoW.pkl')
+#Codifying the training set
+test_data = codec_test(BoW, clusters=30,des_list,n=0)
 
-
-
-clf2 = joblib.load('classifier.pkl')
