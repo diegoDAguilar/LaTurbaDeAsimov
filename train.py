63036334173fa21536e095aa0581377ab7637464
diegoDAguilar <diego.deaguilarcanellas@telefonica.com>
2017-11-21 15:43:03 +0100

    Update train.py

--- train.py
+++ train.py
@@ -23,41 +23,64 @@ images_per_node = 70    #Image dataset size
 #  @brief         Image descriptor generator
 #     
 #  @param         directory         - Numero de clusters en el histograma
-#  @param         des_matrix        - Descriptors matrix 
 #  @param         des_list          - List of matrices, each matrix will contain
-#                                     the descriptors of each image 
+#                                     the descriptors of each image
+#  @param         des_array         - Single matrix containing every matrix
+#                                     from des_list.
 #  @param         sizeX, sizeY      - Image size (default 800x400)
+#  @param         cod_op            - Operation code: 0-Training, 1-Single pic
+#                                     (default 0)
 # 
 #  @return        des_matrix        - Descriptors matrix updated
 #  @return        des_list          - List of matrices updated, each matrix will 
 #                                     contain the descriptors of each image 
 #===========================================================================================================
-def extractor(directory, des_list, des_array, sizeX = 800, sizeY = 400):
-    
-    #Initializations
-    orb = cv2.ORB_create()
-    image_list = os.listdir(directory)
-    
-    #Extracts features from images
-    for i in range(len(image_list)) :
-        #Reads image and sets its size to preset
-        file_name = directory + image_list[i]
-        img = cv2.imread(file_name)
-        #print(file_name)
+
+def extractor(directory, des_list, des_array, sizeX = 800, sizeY = 400, cod_op = 0):
+    if cod_op == 0:
+        #Initializations
+        image_list = os.listdir(directory)
+        #Extracts features from images
+        for i in range(len(image_list)) :
+            #Reads image and sets its size to preset
+            file_name = directory + image_list[i]
+            des = exOrb(file_name, sizeX, sizeY)
+            des_list.append(des)
+            des_array=np.vstack((des_array,des))
+            
+        return des_list, des_array
+            
+    elif cod_op == 1:
+        des = exOrb(directory, sizeX, sizeY)
+        return des, des_array
+        
+#============================================================================================================
+#  Method :       exOrb
+#
+#  @brief         Auxiliary method. Executes ORB
+#     
+#  @param         pic_name          - Path to picture
+#  @param         sizeX, sizeY      - Image size 
+# 
+#  @return        des               - Single image feature descriptors
+#===========================================================================================================
+   
+
+
+def exOrb(pic_name, sizeX, sizeY):
+        orb = cv2.ORB_create()
+        img = cv2.imread(pic_name)
         img = cv2.resize(img, (sizeX, sizeY))
-    
-        #Generates a gray image 
+        
+        #Crea la imagen en escala de grises
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         gray = np.float32(gray)
         
-        # find the keypoints with ORB
+        # Encuentra los puntos claves con ORB
         kp = orb.detect(img,None)
-        # compute the descriptors with ORB
+        # Devuelve los descriptores de cada punto clave
         kp, des = orb.compute(img, kp)
-        des_list.append(des)
-        des_array=np.vstack((des_array,des))
-        
-    return des_list, des_array
+        return des
       
 #============================================================================================================
 #  Method :       codec
@@ -99,7 +122,7 @@ def codec(clusters, des_matrix, des_list, n_images_per_node):
         count+=1
     
     training_data_array = np.delete(training_data_array,0,0)
-    return training_data_array, kmeans
+    return training_data_array
 
 
 #============================================================================================================
@@ -126,6 +149,7 @@ def main(argv):
     #Parameters
     global n_clusters
     global images_per_node
+
     des_list = []  
     des_array = np.zeros((1,32))
     
@@ -138,12 +162,15 @@ def main(argv):
         node_directory = working_dir + '\Dataset\Nodo' + str(i) + '\\'
         des_list, des_array = extractor(node_directory, des_list, des_array)
     des_array = np.delete(des_array,0,0) #Remove the zeros inicialization
+    #Test extractor for one pic
+    #node_directory = 'vlcsnap-2017-11-14-17h55m48s263.png'
+    #des_list, des_array = extractor(node_directory, des_list, des_array, cod_op = 1)
     print('Extractor finished')
     
     #Codifying (BoW) and generating histograms
-    hist_matrix, BoW = codec(n_clusters, des_array, des_list, images_per_node)
+    hist_matrix = codec(n_clusters, des_array, des_list, images_per_node)
     #Save Bag od Words codification
-    joblib.dump(BoW, 'BoW.pkl')
+    np.savez('bag_of_words', BoW = BoW) #Nota: Serge llama a la salida de la codificacion BoW
     #Save training set
     np.savez('training_set', hist_matrix = hist_matrix)
     print('Codec finished')  
