2727a0ec6d61f616d4af960e287b4f045fc12f60
Serge Chávez <30761241+Serge3006@users.noreply.github.com>
2017-11-21 20:24:56 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -12,6 +12,18 @@ from sklearn.externals import joblib
 from train import extractor, codec_test
 import os
 
+#============================================================================================================
+#  @brief         Clasificacion de imagenes nuevas
+#     
+#  Parametros a modificar:
+#
+#  @param         cod_op                - 0: Mas de una imagen / 1: Solo una imagen 
+#  @param         n                     - 0: Mas de una imagen / 1: Solo una imagen
+#
+#  NOTA: Si se tiene una imagen para clasificar la función extractor solo bota la variable: des_list. Si se tiene mas 
+#        imagen la función extractor bota dos variables: des_list y des_array
+#===========================================================================================================
+
 clusters = 8
 des_list = []  
 des_array = np.zeros((1,32))
@@ -22,7 +34,6 @@ node_directory = working_dir + '\Test Images' + '\\'
 '''Main'''
 # Reading training set
 des_list,des_array= extractor(node_directory, des_list, des_array, sizeX = 800, sizeY = 400, cod_op=0)
-# des_array = np.delete(des_array,0,0)
 # Loading the Bag of Words
 BoW = joblib.load('BoW.pkl')
 #Codifying the training set
